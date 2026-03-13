f8b0c38c1425d91e62335f53e2c2c7c1330284b1
Chalim63 <klouda-m@post.cz>
2026-03-13 13:55:14 +0100

    úprava sekce pro aplikace připojené k zařízení

--- src/Entity/Application.php
+++ src/Entity/Application.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\ApplicationRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 
 #[ORM\Entity(repositoryClass: ApplicationRepository::class)]
 class Application
@@ -63,4 +65,44 @@ class Application
 
         return $this;
     }
+
+    #[ORM\ManyToMany(targetEntity: Device::class, mappedBy: 'applications')]
+    private Collection $devices;
+
+    public function __construct()
+    {
+        $this->devices = new ArrayCollection();
+    }
+
+   
+    public function getDevices(): Collection
+    {
+        return $this->devices;
+    }
+
+    public function addDevice(Device $device): static
+    {
+        if (!$this->devices->contains($device)) {
+            $this->devices->add($device);
+            $device->addApplication($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDevice(Device $device): static
+    {
+        if ($this->devices->removeElement($device)) {
+            $device->removeApplication($this);
+        }
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+
+
 }
